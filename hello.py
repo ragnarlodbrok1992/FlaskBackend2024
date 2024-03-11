acc1b79c22ac6677b106be320db4b6d996f1e6e3
ragnarlodbrok1992 <maraus92@gmail.com>
2024-03-11 20:19:32 +0100

    WIP

--- hello.py
+++ hello.py
@@ -12,3 +12,15 @@ def hello_world():
 @app.route("/hello/<name>")
 def hello(name):
     return f"<p>Hello, {escape(name)}!</p>"
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    print(post_id)
+    print(type(post_id))
+    return f"<p>Post with id {escape(post_id)}</p>"
+
+
+@app.route('/post/<post_id>')
+def show_post_incorrect(post_id):
+    return f"<p>Use number for post, not {escape(post_id)}!"
